21275312279e35cdac4c2f85ae0bb9ab4ce0f793
Fu Shiming <louisfsm@163.com>
2023-12-03 16:43:28 +0800

    the case of null is solved

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -80,6 +80,11 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if(A==null)
+        {
+            A=B;
+            return A;
+        }
         if(A.rest==null)
         {
             A.rest = B;
