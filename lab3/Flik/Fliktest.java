2d905129e96aba35c9ac8aa7c26a78b1ea7d352e
Fu Shiming <louisfsm@163.com>
2023-12-03 18:59:38 +0800

    lab3 is finished

---
+++ lab3/Flik/Fliktest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+public class Fliktest {
+    @Test
+    public void testIsSameNumber()
+    {
+        assertTrue(Flik.isSameNumber(128,128));
+    }
+}
--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -69,5 +69,15 @@ public class IntListTest {
     /** If you're running this from the command line, you'll need
       * to add a main method. See ArithmeticTest.java for an
       * example. */
+    @Test(timeout = 1000)
+    public void testReverse()
+    {
+        IntList A = IntList.of(1, 2, 3);
+        IntList B = IntList.reverse(A);
+        assertEquals(IntList.of(3,2,1),B);
+        assertNotEquals(IntList.of(1,2,3),A);
+        IntList C = IntList.reverse(null);
+        assertEquals(null,C);
+    }
 
 }
