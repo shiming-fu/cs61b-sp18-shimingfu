755e1d91e33aee3c672252709b83a0965e96d017
Fu Shiming <louisfsm@163.com>
2023-12-09 23:19:29 +0800

    proj2 is finished

---
+++ proj1b/OffByN.java
@@ -0,0 +1,11 @@
+public class OffByN implements CharacterComparator {
+    public int n;
+    public OffByN(int N){
+        this.n = N;
+    }
+
+    @Override
+    public boolean equalChars(char x, char y) {
+        return false;
+    }
+}
--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -2,6 +2,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestOffByOne {
+    static CharacterComparator offbyone = new OffByOne();
+    @Test
+    public void testEqualChars(){
+        assertTrue(offbyone.equalChars('x','y'));
+        assertTrue(offbyone.equalChars('r','q'));
+        assertTrue(offbyone.equalChars('&','%'));
+
+        assertFalse(offbyone.equalChars('a','c'));
+        assertFalse(offbyone.equalChars('a','a'));
+        assertFalse(offbyone.equalChars('a','A'));
+    }
     /*
     // You must use this CharacterComparator and not instantiate
     // new ones, or the autograder might be upset.
