dace80d0d50222f940e67c4363fdc3ded354f62c
Fu Shiming <louisfsm@163.com>
2024-01-05 21:43:39 +0800

    hw4 version 3

--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -1,72 +1,64 @@
 package hw4.puzzle;
 
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
 
-import java.util.Comparator;
-import java.util.Map;
+
 import java.util.HashMap;
-import java.util.Stack;
 
 public class Solver {
-    private class SearchNode {
-        private WorldState state;
-        private int moves;
-        private Integer priority;
-        private SearchNode prev;
+    private int move;
+    private Stack<WorldState> al;
+    private HashMap<WorldState, Integer> mp = new HashMap<>();
 
-        private SearchNode(WorldState state, SearchNode prev) {
-            this.state = state;
-            this.moves = prev == null ? 0 : prev.moves + 1;
-            if (edCaches.containsKey(this.state)) {
-                int ed = edCaches.get(this.state);
-                this.priority = this.moves + ed;
-            } else {
-                int ed = this.state.estimatedDistanceToGoal();
-                edCaches.put(this.state, ed);
-                this.priority = this.moves + ed;
-            }
-            this.prev = prev;
-        }
-    }
+    public class SearchNode implements Comparable<SearchNode> {
+        private WorldState ws;
+        private int movessofar;
+        private SearchNode pre;
+        private int estimatedD;
 
-    private class SearchNodeComparator implements Comparator<SearchNode> {
         @Override
-        public int compare(SearchNode left, SearchNode right) {
-            return left.priority.compareTo(right.priority);
+        public int compareTo(SearchNode o) {
+            return (this.movessofar + this.estimatedD) - (o.movessofar + o.estimatedD);
+        }
+
+        public SearchNode(WorldState ws, int sofar, SearchNode pre) {
+            this.ws = ws;
+            this.movessofar = sofar;
+            this.pre = pre;
+            if (mp != null & mp.containsKey(ws)) {
+                this.estimatedD = mp.get(ws);
+            } else {
+                this.estimatedD = ws.estimatedDistanceToGoal();
+                mp.put(ws, estimatedD);
+            }
         }
     }
-
-    private Map<WorldState, Integer> edCaches = new HashMap<>();
-    private int searchedCnt = 0; // recording the number of the SearchedNodes are enqueued
-    private Stack<WorldState> path = new Stack<>(); // recording the path
 
     public Solver(WorldState initial) {
-        MinPQ<SearchNode> pq = new MinPQ<>(new SearchNodeComparator());
-        SearchNode currentNode = new SearchNode(initial, null);
-        while (!currentNode.state.isGoal()) {
-            for (WorldState nextState : currentNode.state.neighbors()) {
-                if (currentNode.prev == null || !nextState.equals(currentNode.prev.state)) {
-                    SearchNode nextNode = new SearchNode(nextState, currentNode);
-                    pq.insert(nextNode);
-                    searchedCnt += 1;
+        SearchNode sn = new SearchNode(initial, 0, null);
+        MinPQ<SearchNode> pq = new MinPQ<>();
+        while (!sn.ws.isGoal()) {
+            for (WorldState neighbor : sn.ws.neighbors()) {
+                if (sn.pre == null || !neighbor.equals(sn.pre.ws)) {
+                    pq.insert(new SearchNode(neighbor, sn.movessofar + 1, sn));
                 }
             }
-            currentNode = pq.delMin();
+            sn = pq.delMin();
         }
-        for (SearchNode node = currentNode; node != null; node = node.prev) {
-            path.push(node.state);
+        move = sn.movessofar;
+        al = new Stack<>();
+        while (sn != null) {
+            al.push(sn.ws);
+            sn = sn.pre;
         }
     }
 
     public int moves() {
-        return path.size() - 1;
+        return move;
     }
 
     public Iterable<WorldState> solution() {
-        return path;
-    }
-
-    private int searchedCount() {
-        return searchedCnt;
+        return al;
     }
 }
